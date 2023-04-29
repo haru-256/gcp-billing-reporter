4628c79ae1d7debf0b1da6b2c40d852984521fa6
Yohei Okabayashi <yohei.kuro48@gmail.com>
2023-04-29 19:15:10 +0900

    terraform cloud setting for vcs driven (#1)

--- terraform/tfc-workspace.tf
+++ terraform/tfc-workspace.tf
@@ -9,14 +9,14 @@ resource "tfe_workspace" "workspace" {
   organization        = var.tfc_organization_name
   project_id          = tfe_project.project.id
   speculative_enabled = true
-  # auto_apply          = false
-  # working_directory   = "terraform/"
-  # vcs_repo {
-  #   github_app_installation_id = var.tfc_vcs_repo_ghain
-  #   identifier                 = var.tfc_vcs_repo_identifier
-  #   ingress_submodules         = false
-  #   branch                     = "main"
-  # }
+  auto_apply          = false
+  working_directory   = "terraform"
+  vcs_repo {
+    github_app_installation_id = var.tfc_vcs_repo_ghain
+    identifier                 = var.tfc_vcs_repo_identifier
+    ingress_submodules         = false
+    branch                     = "main"
+  }
 }
 
 resource "tfe_variable" "enable_gcp_provider_auth" {
