d9473e96f3fec307b96ef8f0007efe8249087eb6
Yohei Okabayashi <yohei.kuro48@gmail.com>
2023-04-30 09:07:46 +0900

    wip: for testing github action

---
+++ src/main.py
@@ -0,0 +1,127 @@
+import os
+import pathlib
+from datetime import datetime
+from typing import Any
+
+import pandas as pd
+from dateutil import tz
+from dateutil.relativedelta import relativedelta
+from google.cloud import bigquery
+from slack_sdk.webhook import WebhookClient
+from slack_sdk.webhook.webhook_response import WebhookResponse
+
+
+def calc_gcp_cost(start_date_jst: str, end_date_jst: str) -> tuple[pd.DataFrame, Any]:
+    """Calculate gcp const
+    Args:
+        start_date_jst (str): start date (jst)
+        end_date_jst (str): end date (jst)
+
+    Returns:
+        tuple[pd.DataFrame, Any]: gcp cost dataframe and cost of query
+    """
+    sql_path = pathlib.Path("./sql/calc_gcp_cost.sql")
+    with open(sql_path, "r") as fo:
+        query = fo.read()
+        query = query.format(start_date_jst=start_date_jst, end_date_jst=end_date_jst)
+    bq_client = bigquery.Client(project="haru256-billing-report")
+    query_job = bq_client.query(query)
+    df: pd.DataFrame = query_job.result().to_dataframe()
+    processed_gib_bytes = query_job.total_bytes_processed / 1073741824
+    return df, processed_gib_bytes
+
+
+def build_message(
+    start_date_jst: str, end_date_jst: str, cost_df: pd.DataFrame, processed_gib_bytes: int
+) -> list[dict[str, Any]]:
+    """Build message that is sent to slack
+
+    Args:
+        start_date_jst (str): start date (jst)
+        end_date_jst (str): end date (jst)
+        cost_df (pd.DataFrame): gcp cost dataframe
+        processed_gib_bytes (int): cost of calculating query
+
+    Returns:
+        list[dict[str, Any]]: slack message blocks
+    """
+    if len(cost_df) == 0:
+        cost_text = "No Billing Cost."
+    else:
+        cost_text = "\n".join(
+            [f"• {row.service_name}: {row.total:.2f} yen" for _, row in cost_df.iterrows()]
+        )
+    billing_report_url = "https://console.cloud.google.com/billing/01D25E-D97B0C-ADC50F"
+    blocks = [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": f"`Daily Report` (2 week cost from *{start_date_jst}* to *{end_date_jst}*) "
+                ":money_with_wings:",
+            },
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": f"Billing Cost from BigQuery (processed GiB: {processed_gib_bytes:.2f})",
+            },
+        },
+        {"type": "section", "text": {"type": "mrkdwn", "text": cost_text}},
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "More Info:\n" f"• <{billing_report_url}|Billing Report>\n",
+            },
+        },
+    ]
+
+    return blocks
+
+
+def report_gcp_cost_to_slack(slack_webhook_url: str) -> WebhookResponse:
+    """Reporting GCP cost to the Slack channel
+    Args:
+        slack_webhook_url (str): webhook url
+    Returns:
+        WebhookResponse: the response from the slack webhook
+    """
+    webhook = WebhookClient(slack_webhook_url)
+
+    end_datetime_jst = datetime.now(tz=tz.gettz("Asia/Tokyo"))
+    end_date_jst = end_datetime_jst.strftime("%Y-%m-%d")
+    start_datetime_jst = end_datetime_jst - relativedelta(weeks=2)
+    start_date_jst = start_datetime_jst.strftime("%Y-%m-%d")
+
+    cost_df, processed_gib_bytes = calc_gcp_cost(start_date_jst, end_date_jst)
+    blocks = build_message(start_date_jst, end_date_jst, cost_df, processed_gib_bytes)
+    response = webhook.send(
+        text="Billing Report",
+        blocks=blocks,
+    )
+    return response
+
+
+def main(
+    msg: str,
+    context: str,
+) -> WebhookResponse:
+    """Endpoint for google cloud function.
+    Args:
+        msg (str): message from Pub/Sub trigger
+        context (str): context from Pub/Sub trigger
+    Returns:
+        WebhookResponse: the response from the slack webhook
+    """
+    slack_webhook_url = os.getenv("SLACK_WEBHOOK_URL")
+    assert (
+        slack_webhook_url is not None
+    ), "environmental variable: 'SLACK_WEBHOOK_URL' must be not None."
+
+    return report_gcp_cost_to_slack(slack_webhook_url)
+
+
+if __name__ == "__main__":
+    main("test", "test")
---
+++ terraform/billing-reporter.tf
@@ -0,0 +1,97 @@
+# declare resources for billing reporter
+
+# bigquery dataset that GCP export billing data to
+resource "google_bigquery_dataset" "all_billing_data" {
+  location                   = var.gcp_default_region
+  project                    = var.gcp_project_id
+  dataset_id                 = "all_billing_data"
+  description                = "Storing billing data for all google cloud project"
+  delete_contents_on_destroy = "false"
+
+  access {
+    role          = "OWNER"
+    special_group = "projectOwners"
+  }
+  access {
+    role          = "READER"
+    special_group = "projectReaders"
+  }
+  access {
+    role          = "WRITER"
+    special_group = "projectWriters"
+  }
+  access {
+    role          = "OWNER"
+    user_by_email = "billing-export-bigquery@system.gserviceaccount.com"
+  }
+
+}
+
+# pubsub that cloud scheduler push to
+resource "google_pubsub_topic" "billing_reporter" {
+  name = "billing_reporter"
+}
+
+# to store cloud function logs
+resource "google_storage_bucket" "billing_reporter_cloud_function_logs" {
+  name     = "${var.gcp_project_id}-billing-reporter-cloud-function-logs"
+  location = var.gcp_default_region
+}
+
+# run scheduler job of billing report
+resource "google_cloud_scheduler_job" "billing_reporter" {
+  description = "Billing report to slack"
+  name        = "billing-reporter"
+
+  pubsub_target {
+    data       = base64encode("billing-reporter")
+    topic_name = google_pubsub_topic.billing_reporter.id
+  }
+
+  retry_config {
+    max_backoff_duration = "3600s"
+    max_doublings        = "5"
+    max_retry_duration   = "0s"
+    min_backoff_duration = "5s"
+    retry_count          = "0"
+  }
+
+  schedule  = "0 7 * * *"
+  time_zone = "Asia/Tokyo"
+}
+
+# Use this secret in cloud run endpoint
+resource "google_secret_manager_secret" "slack_webhook_url" {
+  secret_id = "SLACK_WEBHOOK_URL"
+  replication {
+    automatic = true
+  }
+}
+resource "google_secret_manager_secret_version" "slack_webhook_url" {
+  secret = google_secret_manager_secret.slack-webhook-url.id
+
+  secret_data = var.slack_webhook_url
+}
+
+# billing reporter service account
+resource "google_service_account" "billing_reporter" {
+  project      = var.gcp_project_id
+  account_id   = "billing_reporter"
+  display_name = "Billing Reporter Service Account"
+  description  = "Billing Report Service Account."
+}
+resource "google_project_iam_member" "billing-report" {
+  project = var.project_id
+  for_each = toset([
+    "roles/bigquery.dataViewer",
+    "roles/bigquery.jobUser",
+    "roles/bigquery.readSessionUser",
+    "roles/cloudbuild.builds.builder",
+    "roles/cloudfunctions.developer",
+    "roles/logging.logWriter",
+    "roles/storage.admin",
+    "roles/iam.serviceAccountUser"
+  ])
+  role   = each.key
+  member = "serviceAccount:${google_service_account.billing_reporter.email}"
+}
