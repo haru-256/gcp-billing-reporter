e77db6b9de15169816ef426dbf87822debdd7b57
Yohei Okabayashi <yohei.kuro48@gmail.com>
2023-04-30 20:29:21 +0900

    delete

--- terraform/gh-repository.tf
+++ terraform/gh-repository.tf
@@ -5,5 +5,29 @@ data "github_repository" "gcp_billing_reporter" {
 resource "github_actions_variable" "gcp_project_id" {
   repository    = data.github_repository.gcp_billing_reporter.name
   variable_name = "GCP_PROJECT_ID"
-  value         = "haru256-billing-report"
+  value         = var.gcp_project_id
+}
+
+resource "github_actions_variable" "gh_gcp_workload_identity_provider" {
+  repository    = data.github_repository.gcp_billing_reporter.name
+  variable_name = "GH_GCP_WORKLOAD_IDENTITY_PROVIDER"
+  value         = module.gh_oidc.provider_name
+}
+
+resource "github_actions_variable" "gh_gcp_service_account" {
+  repository    = data.github_repository.gcp_billing_reporter.name
+  variable_name = "GH_GCP_SERVICE_ACCOUNT"
+  value         = google_service_account.gh.email
+}
+
+resource "github_actions_variable" "billing_reporter_gcp_service_account" {
+  repository    = data.github_repository.gcp_billing_reporter.name
+  variable_name = "BILLING_REPORTER_GCP_SERVICE_ACCOUNT"
+  value         = google_service_account.billing_reporter.email
+}
+
+resource "github_actions_variable" "billing_reporter_pubsub_topic" {
+  repository    = data.github_repository.gcp_billing_reporter.name
+  variable_name = "BILLING_REPORTER_PUBSUB_TOPIC"
+  value         = google_pubsub_topic.billing_reporter.id
 }
