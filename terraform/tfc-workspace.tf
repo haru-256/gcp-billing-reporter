3e0bc3fa649188091572de920714bfd34435b7d3
Yohei Okabayashi <yohei.kuro48@gmail.com>
2023-05-06 00:47:21 +0900

    ADD: add slack notification (#12)

--- terraform/tfc-workspace.tf
+++ terraform/tfc-workspace.tf
@@ -18,3 +18,13 @@ resource "tfe_workspace" "workspace" {
     branch                     = "main"
   }
 }
+
+# slack notification
+resource "tfe_notification_configuration" "slack" {
+  name             = "slack"
+  enabled          = true
+  destination_type = "slack"
+  triggers         = ["run:created", "run:planning", "run:needs_attention", "run:applying", "run:completed", "run:errored"]
+  url              = var.tfc_slack_webhook_url
+  workspace_id     = tfe_workspace.workspace.id
+}
