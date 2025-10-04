92db359cb06038e0c3322474c9653a0d2bb341b7
Yohei Okabayashi <yohei.kuro48@gmail.com>
2025-10-04 23:29:30 +0900

    Build(terraform): refactor gcp_services to use toset and update resource configuration

--- terraform/gcp-services.tf
+++ terraform/gcp-services.tf
@@ -1,22 +1,21 @@
 # declare resource for gcp services
 
 locals {
-  gcp_services = [
+  gcp_services = toset([
     "iam.googleapis.com",
     "cloudresourcemanager.googleapis.com",
     "sts.googleapis.com",
     "iamcredentials.googleapis.com",
-    "cloudresourcemanager.googleapis.com",
     "secretmanager.googleapis.com",
     "cloudscheduler.googleapis.com",
     "cloudfunctions.googleapis.com",
     "cloudbuild.googleapis.com",
-    "eventarc.googleapis.com" # for cloud function 2nd gen
-  ]
+    "eventarc.googleapis.com"
+  ])
 }
 
 resource "google_project_service" "services" {
-  project = var.gcp_project_id
-  count   = length(local.gcp_services)
-  service = local.gcp_services[count.index]
+  project  = var.gcp_project_id
+  for_each = local.gcp_services
+  service  = each.key
 }
