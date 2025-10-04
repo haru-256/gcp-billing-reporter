ce2193c45cc9b1721d88d11d0751a8db65642c22
Yohei Okabayashi <yohei.kuro48@gmail.com>
2025-10-05 00:18:15 +0900

    Uncomment roles for Cloud Functions Service Agent in IAM configuration

--- terraform/gcp-member-iam.tf
+++ terraform/gcp-member-iam.tf
@@ -16,11 +16,11 @@ resource "google_project_iam_member" "owner_member" {
 resource "google_project_iam_member" "cloud_function_service_agent" {
   project = var.gcp_project_id
   for_each = toset([
-    # "roles/cloudfunctions.serviceAgent",       # Cloud Functions の実行に必須
-    # "roles/iam.serviceAccountUser",            # ランタイム用 SA の利用権限
-    # "roles/iam.serviceAccountTokenCreator",    # OIDC 等のトークン作成で必要になることがある
+    "roles/cloudfunctions.serviceAgent",       # Cloud Functions の実行に必須
+    "roles/iam.serviceAccountUser",            # ランタイム用 SA の利用権限
+    "roles/iam.serviceAccountTokenCreator",    # OIDC 等のトークン作成で必要になることがある
     "roles/storage.objectAdmin",               # gcf-v2-uploads バケットへの書き込み権限
-    # "roles/storage.legacyBucketReader"         # バケットのメタデータ参照
+    "roles/storage.legacyBucketReader"         # バケットのメタデータ参照
   ])
   role   = each.key
   member = "serviceAccount:${var.gcp_cloud_function_service_agent}"
